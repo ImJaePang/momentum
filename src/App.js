fcaf1aec8f19de861db73fc56181e5372eb8baf4
ImJaePang <narushel@naver.com>
2024-06-20 23:47:44 +0900

    app toDos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import {useState} from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event)=> setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();  
+    if (toDo === ""){
+      return;
+    }
+    console.log(toDo);
+    setToDos(currentArray => [toDo, ...toDos]);
+    setToDo("");
+  }
+  console.log(toDos);
   return (
-    <div >
-      <h1>Welcome Back</h1>
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form>
+        <input
+          onChange={onChange}
+          value={toDo} 
+          type="text" 
+          placeholder="Write your to do..."
+        />
+        <button onClick={onSubmit}>Add To Do</button>
+      </form>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import App from './App';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById("root")
 );
 
